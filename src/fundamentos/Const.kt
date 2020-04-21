a83e5377127595074426822fa351aa0a75a2442f
Mateus <mateus3m3@gmail.com>
2020-04-21 17:31:50 -0300

    Segundo commit da aula

---
+++ src/fundamentos/Const.kt
@@ -0,0 +1,13 @@
+package fundamentos
+
+//Demosntração de constante já reservada
+
+fun main(args: Array<String>) {
+    val a: Int = 1
+    val b = 2 //Tipo Inserido
+
+    //a = a + b
+
+    print(a)
+
+}
---
+++ src/fundamentos/CuidadoInferencia1.kt
@@ -0,0 +1,10 @@
+package fundamentos
+
+fun main(args: Array<String>) {
+    //var a //Deixei com erro pra mostrar que não está certo.
+    var b = 2
+
+    //a=10
+
+    //print(a + b)
+}
